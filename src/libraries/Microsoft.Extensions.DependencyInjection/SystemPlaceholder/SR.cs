c4b7114c78979577136b2a23b9df03e9e59be344
wvpm <24685035+wvpm@users.noreply.github.com>
2024-08-04 12:55:55 +0200

    All projects except specification test buildable

---
+++ src/libraries/Microsoft.Extensions.DependencyInjection/SystemPlaceholder/SR.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+
+namespace System;
+
+public static class SR {
+	public const string
+	AmbiguousConstructorException = "AmbiguousConstructorException",
+	AotCannotCreateEnumerableValueType = "AotCannotCreateEnumerableValueType",
+	AotCannotCreateGenericValueType = "AotCannotCreateGenericValueType",
+	ArityOfOpenGenericServiceNotEqualArityOfOpenGenericImplementation = "ArityOfOpenGenericServiceNotEqualArityOfOpenGenericImplementation",
+	AsyncDisposableServiceDispose = "AsyncDisposableServiceDispose",
+	CallSiteTypeNotSupported = "CallSiteTypeNotSupported",
+	CannotCreateAbstractClasses = "CannotCreateAbstractClasses",
+	CannotResolveService = "CannotResolveService",
+	CircularDependencyException = "CircularDependencyException",
+	ConstantCantBeConvertedToServiceType = "ConstantCantBeConvertedToServiceType",
+	CtorNotLocated = "CtorNotLocated",
+	DirectScopedResolvedFromRootException = "DirectScopedResolvedFromRootException",
+	GetCaptureDisposableNotSupported = "GetCaptureDisposableNotSupported",
+	ImplementationTypeCantBeConvertedToServiceType = "ImplementationTypeCantBeConvertedToServiceType",
+	InvalidServiceDescriptor = "InvalidServiceDescriptor",
+	InvalidServiceKeyType = "InvalidServiceKeyType",
+	KeyedDescriptorMisuse = "KeyedDescriptorMisuse",
+	KeyedServicesNotSupported = "KeyedServicesNotSupported",
+	MarkedCtorMissingArgumentTypes = "MarkedCtorMissingArgumentTypes",
+	MultipleCtorsFound = "MultipleCtorsFound",
+	MultipleCtorsFoundWithBestLength = "MultipleCtorsFoundWithBestLength",
+	MultipleCtorsMarkedWithAttribute = "MultipleCtorsMarkedWithAttribute",
+	NoConstructorMatch = "NoConstructorMatch",
+	NonKeyedDescriptorMisuse = "NonKeyedDescriptorMisuse",
+	NoServiceRegistered = "NoServiceRegistered",
+	OpenGenericServiceRequiresOpenGenericImplementation = "OpenGenericServiceRequiresOpenGenericImplementation",
+	ScopedInSingletonException = "ScopedInSingletonException",
+	ScopedResolvedFromRootException = "ScopedResolvedFromRootException",
+	ServiceCollectionReadOnly = "ServiceCollectionReadOnly",
+	ServiceDescriptorNotExist = "ServiceDescriptorNotExist",
+	TrimmingAnnotationsDoNotMatch = "TrimmingAnnotationsDoNotMatch",
+	TrimmingAnnotationsDoNotMatch_NewConstraint = "TrimmingAnnotationsDoNotMatch_NewConstraint",
+	TryAddIndistinguishableTypeToEnumerable = "TryAddIndistinguishableTypeToEnumerable",
+	TypeCannotBeActivated = "TypeCannotBeActivated",
+	UnableToActivateTypeException = "UnableToActivateTypeException",
+	UnableToResolveService = "UnableToResolveService";
+
+	public static string Format(string id, params object?[] parameters) {
+		return id + parameters.Select(x => " " + x?.ToString() ?? "[NULL]");
+	}
+}
---
+++ src/libraries/Microsoft.Extensions.DependencyInjection/SystemPlaceholder/ThrowHelper.cs
@@ -0,0 +1,8 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace System;
+
+public static class ThrowHelper {
+	public static void ThrowIfNull([NotNull] object? argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null) => ArgumentNullException.ThrowIfNull(argument, paramName);
+}
