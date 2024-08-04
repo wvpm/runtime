d70b8e4e4e0f46dcb2a94f35992274a26f1b3731
wvpm <24685035+wvpm@users.noreply.github.com>
2024-08-04 16:07:06 +0200

    Revert "Fixed deadlock due to circular dependencies"
    This reverts commit 9df67cb23a64965fc6e5bf97752d624bc693ef5c.

--- src/libraries/Microsoft.Extensions.DependencyInjection/src/ServiceLookup/ServiceProviderEngineScope.cs
+++ src/libraries/Microsoft.Extensions.DependencyInjection/src/ServiceLookup/ServiceProviderEngineScope.cs
@@ -8,233 +8,254 @@ using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Internal;
 
-namespace Microsoft.Extensions.DependencyInjection.ServiceLookup;
+namespace Microsoft.Extensions.DependencyInjection.ServiceLookup
+{
+    [DebuggerDisplay("{DebuggerToString(),nq}")]
+    [DebuggerTypeProxy(typeof(ServiceProviderEngineScopeDebugView))]
+    internal sealed class ServiceProviderEngineScope : IServiceScope, IServiceProvider, IKeyedServiceProvider, IAsyncDisposable, IServiceScopeFactory
+    {
+        // For testing and debugging only
+        internal IList<object> Disposables => _disposables ?? (IList<object>)Array.Empty<object>();
 
-[DebuggerDisplay("{DebuggerToString(),nq}")]
-[DebuggerTypeProxy(typeof(ServiceProviderEngineScopeDebugView))]
-internal sealed class ServiceProviderEngineScope : IServiceScope, IServiceProvider, IKeyedServiceProvider, IAsyncDisposable, IServiceScopeFactory {
-	private readonly HashSet<Type> _tryingToResolve = [];
+        private bool _disposed;
+        private List<object>? _disposables;
 
-	// For testing and debugging only
-	internal IList<object> Disposables => _disposables ?? (IList<object>)Array.Empty<object>();
+        public ServiceProviderEngineScope(ServiceProvider provider, bool isRootScope)
+        {
+            ResolvedServices = new Dictionary<ServiceCacheKey, object?>();
+            RootProvider = provider;
+            IsRootScope = isRootScope;
+        }
 
-	private bool _disposed;
-	private List<object>? _disposables;
+        internal Dictionary<ServiceCacheKey, object?> ResolvedServices { get; }
 
-	public ServiceProviderEngineScope(ServiceProvider provider, bool isRootScope) {
-		ResolvedServices = new Dictionary<ServiceCacheKey, object?>();
-		RootProvider = provider;
-		IsRootScope = isRootScope;
-	}
+        internal bool Disposed => _disposed;
 
-	internal Dictionary<ServiceCacheKey, object?> ResolvedServices { get; }
+        // This lock protects state on the scope, in particular, for the root scope, it protects
+        // the list of disposable entries only, since ResolvedServices are cached on CallSites
+        // For other scopes, it protects ResolvedServices and the list of disposables
+        internal object Sync => ResolvedServices;
 
-	internal bool Disposed => _disposed;
+        public bool IsRootScope { get; }
 
-	// This lock protects state on the scope, in particular, for the root scope, it protects
-	// the list of disposable entries only, since ResolvedServices are cached on CallSites
-	// For other scopes, it protects ResolvedServices and the list of disposables
-	internal object Sync => ResolvedServices;
+        internal ServiceProvider RootProvider { get; }
 
-	public bool IsRootScope { get; }
+        public object? GetService(Type serviceType)
+        {
+            if (_disposed)
+            {
+                ThrowHelper.ThrowObjectDisposedException();
+            }
 
-	internal ServiceProvider RootProvider { get; }
+            return RootProvider.GetService(ServiceIdentifier.FromServiceType(serviceType), this);
+        }
 
-	public object? GetService(Type serviceType) {
-		if (_disposed) {
-			ThrowHelper.ThrowObjectDisposedException();
-		}
+        public object? GetKeyedService(Type serviceType, object? serviceKey)
+        {
+            if (_disposed)
+            {
+                ThrowHelper.ThrowObjectDisposedException();
+            }
 
-		using (GuardAgainstCircularDependency(serviceType)) {
-			return RootProvider.GetService(ServiceIdentifier.FromServiceType(serviceType), this);
-		}
-	}
+            return RootProvider.GetKeyedService(serviceType, serviceKey, this);
+        }
 
-	public object? GetKeyedService(Type serviceType, object? serviceKey) {
-		if (_disposed) {
-			ThrowHelper.ThrowObjectDisposedException();
-		}
+        public object GetRequiredKeyedService(Type serviceType, object? serviceKey)
+        {
+            if (_disposed)
+            {
+                ThrowHelper.ThrowObjectDisposedException();
+            }
 
-		using (GuardAgainstCircularDependency(serviceType)) {
-			return RootProvider.GetKeyedService(serviceType, serviceKey, this);
-		}
-	}
+            return RootProvider.GetRequiredKeyedService(serviceType, serviceKey, this);
+        }
 
-	public object GetRequiredKeyedService(Type serviceType, object? serviceKey) {
-		if (_disposed) {
-			ThrowHelper.ThrowObjectDisposedException();
-		}
+        public IServiceProvider ServiceProvider => this;
 
-		using (GuardAgainstCircularDependency(serviceType)) {
-			return RootProvider.GetRequiredKeyedService(serviceType, serviceKey, this);
-		}
-	}
+        public IServiceScope CreateScope() => RootProvider.CreateScope();
 
-	public IServiceProvider ServiceProvider => this;
+        [return: NotNullIfNotNull(nameof(service))]
+        internal object? CaptureDisposable(object? service)
+        {
+            if (ReferenceEquals(this, service) || !(service is IDisposable || service is IAsyncDisposable))
+            {
+                return service;
+            }
 
-	public IServiceScope CreateScope() => RootProvider.CreateScope();
+            bool disposed = false;
+            lock (Sync)
+            {
+                if (_disposed)
+                {
+                    disposed = true;
+                }
+                else
+                {
+                    _disposables ??= new List<object>();
 
-	[return: NotNullIfNotNull(nameof(service))]
-	internal object? CaptureDisposable(object? service) {
-		if (ReferenceEquals(this, service) || !(service is IDisposable || service is IAsyncDisposable)) {
-			return service;
-		}
+                    _disposables.Add(service);
+                }
+            }
 
-		bool disposed = false;
-		lock (Sync) {
-			if (_disposed) {
-				disposed = true;
-			}
-			else {
-				_disposables ??= new List<object>();
+            // Don't run customer code under the lock
+            if (disposed)
+            {
+                if (service is IDisposable disposable)
+                {
+                    disposable.Dispose();
+                }
+                else
+                {
+                    // sync over async, for the rare case that an object only implements IAsyncDisposable and may end up starving the thread pool.
+                    object? localService = service; // copy to avoid closure on other paths
+                    Task.Run(() => ((IAsyncDisposable)localService).DisposeAsync().AsTask()).GetAwaiter().GetResult();
+                }
 
-				_disposables.Add(service);
-			}
-		}
+                ThrowHelper.ThrowObjectDisposedException();
+            }
 
-		// Don't run customer code under the lock
-		if (disposed) {
-			if (service is IDisposable disposable) {
-				disposable.Dispose();
-			}
-			else {
-				// sync over async, for the rare case that an object only implements IAsyncDisposable and may end up starving the thread pool.
-				object? localService = service; // copy to avoid closure on other paths
-				Task.Run(() => ((IAsyncDisposable)localService).DisposeAsync().AsTask()).GetAwaiter().GetResult();
-			}
+            return service;
+        }
 
-			ThrowHelper.ThrowObjectDisposedException();
-		}
+        public void Dispose()
+        {
+            List<object>? toDispose = BeginDispose();
 
-		return service;
-	}
+            if (toDispose != null)
+            {
+                for (int i = toDispose.Count - 1; i >= 0; i--)
+                {
+                    if (toDispose[i] is IDisposable disposable)
+                    {
+                        disposable.Dispose();
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException(SR.Format(SR.AsyncDisposableServiceDispose, TypeNameHelper.GetTypeDisplayName(toDispose[i])));
+                    }
+                }
+            }
+        }
 
-	public void Dispose() {
-		List<object>? toDispose = BeginDispose();
+        public ValueTask DisposeAsync()
+        {
+            List<object>? toDispose = BeginDispose();
 
-		if (toDispose != null) {
-			for (int i = toDispose.Count - 1; i >= 0; i--) {
-				if (toDispose[i] is IDisposable disposable) {
-					disposable.Dispose();
-				}
-				else {
-					throw new InvalidOperationException(SR.Format(SR.AsyncDisposableServiceDispose, TypeNameHelper.GetTypeDisplayName(toDispose[i])));
-				}
-			}
-		}
-	}
+            if (toDispose != null)
+            {
+                try
+                {
+                    for (int i = toDispose.Count - 1; i >= 0; i--)
+                    {
+                        object disposable = toDispose[i];
+                        if (disposable is IAsyncDisposable asyncDisposable)
+                        {
+                            ValueTask vt = asyncDisposable.DisposeAsync();
+                            if (!vt.IsCompletedSuccessfully)
+                            {
+                                return Await(i, vt, toDispose);
+                            }
 
-	public ValueTask DisposeAsync() {
-		List<object>? toDispose = BeginDispose();
+                            // If its a IValueTaskSource backed ValueTask,
+                            // inform it its result has been read so it can reset
+                            vt.GetAwaiter().GetResult();
+                        }
+                        else
+                        {
+                            ((IDisposable)disposable).Dispose();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return new ValueTask(Task.FromException(ex));
+                }
+            }
 
-		if (toDispose != null) {
-			try {
-				for (int i = toDispose.Count - 1; i >= 0; i--) {
-					object disposable = toDispose[i];
-					if (disposable is IAsyncDisposable asyncDisposable) {
-						ValueTask vt = asyncDisposable.DisposeAsync();
-						if (!vt.IsCompletedSuccessfully) {
-							return Await(i, vt, toDispose);
-						}
+            return default;
 
-						// If its a IValueTaskSource backed ValueTask,
-						// inform it its result has been read so it can reset
-						vt.GetAwaiter().GetResult();
-					}
-					else {
-						((IDisposable)disposable).Dispose();
-					}
-				}
-			}
-			catch (Exception ex) {
-				return new ValueTask(Task.FromException(ex));
-			}
-		}
+            static async ValueTask Await(int i, ValueTask vt, List<object> toDispose)
+            {
+                await vt.ConfigureAwait(false);
+                // vt is acting on the disposable at index i,
+                // decrement it and move to the next iteration
+                i--;
 
-		return default;
+                for (; i >= 0; i--)
+                {
+                    object disposable = toDispose[i];
+                    if (disposable is IAsyncDisposable asyncDisposable)
+                    {
+                        await asyncDisposable.DisposeAsync().ConfigureAwait(false);
+                    }
+                    else
+                    {
+                        ((IDisposable)disposable).Dispose();
+                    }
+                }
+            }
+        }
 
-		static async ValueTask Await(int i, ValueTask vt, List<object> toDispose) {
-			await vt.ConfigureAwait(false);
-			// vt is acting on the disposable at index i,
-			// decrement it and move to the next iteration
-			i--;
+        private List<object>? BeginDispose()
+        {
+            lock (Sync)
+            {
+                if (_disposed)
+                {
+                    return null;
+                }
 
-			for (; i >= 0; i--) {
-				object disposable = toDispose[i];
-				if (disposable is IAsyncDisposable asyncDisposable) {
-					await asyncDisposable.DisposeAsync().ConfigureAwait(false);
-				}
-				else {
-					((IDisposable)disposable).Dispose();
-				}
-			}
-		}
-	}
+                // Track statistics about the scope (number of disposable objects and number of disposed services)
+                DependencyInjectionEventSource.Log.ScopeDisposed(RootProvider.GetHashCode(), ResolvedServices.Count, _disposables?.Count ?? 0);
 
-	private List<object>? BeginDispose() {
-		lock (Sync) {
-			if (_disposed) {
-				return null;
-			}
+                // We've transitioned to the disposed state, so future calls to
+                // CaptureDisposable will immediately dispose the object.
+                // No further changes to _state.Disposables, are allowed.
+                _disposed = true;
 
-			// Track statistics about the scope (number of disposable objects and number of disposed services)
-			DependencyInjectionEventSource.Log.ScopeDisposed(RootProvider.GetHashCode(), ResolvedServices.Count, _disposables?.Count ?? 0);
+            }
 
-			// We've transitioned to the disposed state, so future calls to
-			// CaptureDisposable will immediately dispose the object.
-			// No further changes to _state.Disposables, are allowed.
-			_disposed = true;
+            if (IsRootScope && !RootProvider.IsDisposed())
+            {
+                // If this ServiceProviderEngineScope instance is a root scope, disposing this instance will need to dispose the RootProvider too.
+                // Otherwise the RootProvider will never get disposed and will leak.
+                // Note, if the RootProvider get disposed first, it will automatically dispose all attached ServiceProviderEngineScope objects.
+                RootProvider.Dispose();
+            }
 
-		}
+            // ResolvedServices is never cleared for singletons because there might be a compilation running in background
+            // trying to get a cached singleton service. If it doesn't find it
+            // it will try to create a new one which will result in an ObjectDisposedException.
+            return _disposables;
+        }
 
-		if (IsRootScope && !RootProvider.IsDisposed()) {
-			// If this ServiceProviderEngineScope instance is a root scope, disposing this instance will need to dispose the RootProvider too.
-			// Otherwise the RootProvider will never get disposed and will leak.
-			// Note, if the RootProvider get disposed first, it will automatically dispose all attached ServiceProviderEngineScope objects.
-			RootProvider.Dispose();
-		}
+        internal string DebuggerToString()
+        {
+            string debugText = $"ServiceDescriptors = {RootProvider.CallSiteFactory.Descriptors.Length}";
+            if (!IsRootScope)
+            {
+                debugText += $", IsScope = true";
+            }
+            if (_disposed)
+            {
+                debugText += $", Disposed = true";
+            }
+            return debugText;
+        }
 
-		// ResolvedServices is never cleared for singletons because there might be a compilation running in background
-		// trying to get a cached singleton service. If it doesn't find it
-		// it will try to create a new one which will result in an ObjectDisposedException.
-		return _disposables;
-	}
+        private sealed class ServiceProviderEngineScopeDebugView
+        {
+            private readonly ServiceProviderEngineScope _serviceProvider;
 
-	internal string DebuggerToString() {
-		string debugText = $"ServiceDescriptors = {RootProvider.CallSiteFactory.Descriptors.Length}";
-		if (!IsRootScope) {
-			debugText += $", IsScope = true";
-		}
-		if (_disposed) {
-			debugText += $", Disposed = true";
-		}
-		return debugText;
-	}
+            public ServiceProviderEngineScopeDebugView(ServiceProviderEngineScope serviceProvider)
+            {
+                _serviceProvider = serviceProvider;
+            }
 
-	private sealed class ServiceProviderEngineScopeDebugView {
-		private readonly ServiceProviderEngineScope _serviceProvider;
-
-		public ServiceProviderEngineScopeDebugView(ServiceProviderEngineScope serviceProvider) {
-			_serviceProvider = serviceProvider;
-		}
-
-		public List<ServiceDescriptor> ServiceDescriptors => new(_serviceProvider.RootProvider.CallSiteFactory.Descriptors);
-		public List<object> Disposables => new(_serviceProvider.Disposables);
-		public bool Disposed => _serviceProvider._disposed;
-		public bool IsScope => !_serviceProvider.IsRootScope;
-	}
-
-	private IDisposable GuardAgainstCircularDependency(Type serviceType) {
-		if (!_tryingToResolve.Add(serviceType)) {
-			throw new InvalidOperationException(SR.Format(SR.CircularDependencyException, serviceType));
-		}
-
-		return new SetRemover(serviceType, _tryingToResolve);
-	}
-
-	private sealed class SetRemover(Type _type, HashSet<Type> _tryingToResolve) : IDisposable {
-		public void Dispose() {
-			_tryingToResolve.Remove(_type);
-		}
-	}
+            public List<ServiceDescriptor> ServiceDescriptors => new List<ServiceDescriptor>(_serviceProvider.RootProvider.CallSiteFactory.Descriptors);
+            public List<object> Disposables => new List<object>(_serviceProvider.Disposables);
+            public bool Disposed => _serviceProvider._disposed;
+            public bool IsScope => !_serviceProvider.IsRootScope;
+        }
+    }
 }
